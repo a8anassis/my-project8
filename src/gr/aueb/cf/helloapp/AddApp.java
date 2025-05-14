70109129bfb49a68cd9f583a5546aa3876d28347
a8anassis <a8anassis@gmail.com>
2025-05-14 20:22:45 +0300

    fix: add print formatted

--- src/gr/aueb/cf/helloapp/AddApp.java
+++ src/gr/aueb/cf/helloapp/AddApp.java
@@ -3,6 +3,7 @@ package gr.aueb.cf.helloapp;
 public class AddApp {
     public static void main(String[] args) {
         System.out.println(add(3, 5));
+        System.out.printf("%d", add(3, 5));
     }
 
     public static int add(int a, int b) {
